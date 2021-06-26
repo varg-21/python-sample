8cf14f8405249a08c66752764208151fe85a61a1
varg-21 <gowthambmn@gmail.com>
2021-06-26 15:17:50 +0530

    First commit of my first python application.

---
+++ madlibs.py
@@ -0,0 +1,9 @@
+place1 = input("A place: ")
+adjective = input("Adj.: ")
+place2 = input("Another place: ")
+object1 = input("Any object: ")
+
+madlib = print(f"I have been to {place1}, it is a/an {adjective} place. \
+I want to visit {place2}, to explore the hidden {object1} which is very famous there.")
+
+print(madlib)
